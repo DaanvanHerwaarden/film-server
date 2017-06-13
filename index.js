c75fde62541c1ddaa6b06079cb4088b4c07f745b
Mats Gemmeke <matsgemmeke@gmail.com>
2017-06-13 12:36:42 +0200

    Config file toegevoegd

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+var config = require("./config/config.json");
 var express = require("express");
 
 var app = express();
 
+// Fallback - als geen enkele andere route slaagt wordt deze uitgevoerd. 
+app.use('*', function(req, res) {
+    res.status(400);
+    res.json({
+        "error": "Deze URL is niet beschikbaar."
+    });
+});
+
 app.listen(3000, function() {
-    console.log("Server luistert op port 3000");
+    console.log("Server luistert op port " + config.port);
 });
