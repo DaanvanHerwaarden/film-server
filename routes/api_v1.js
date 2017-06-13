a8ce5d59d608bb77969a703ae2743d3ee2ceaeb8
Mats Gemmeke <matsgemmeke@gmail.com>
2017-06-13 15:12:44 +0200

    Films offsets gefixt

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -2,6 +2,22 @@ var express = require("express");
 var router = express.Router();
 var database = require("../database.js");
 
+router.get("/films", function (req, res) {
+	res.contentType("application/json");
+	
+	var offset = parseInt(req.query.offset);
+	var count = parseInt(req.query.count);
+	
+	database.query("SELECT * FROM `film` LIMIT ? OFFSET ?", [ count, offset ], function(error, rows, fields) {
+		if (error) {
+			res.status(400).json(error);
+			console.log(error);
+		} else {
+			res.status(200).json(rows);
+		}
+	});
+});
+
 router.get("/films/:filmid", function(req, res) {
 	res.contentType("application/json");
 
@@ -17,7 +33,7 @@ router.get("/films/:filmid", function(req, res) {
 });
 
 router.get("/login", function(req, res) {
-	res.contentType('application/json');
+	res.contentType("application/json");
 	res.status(200).json({
 		"test": "success"
 	});
