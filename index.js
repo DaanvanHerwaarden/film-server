005d502288a2bb341e253faad93cea3169db7f88
Mats Gemmeke <matsgemmeke@gmail.com>
2017-06-13 13:03:11 +0200

    API route toegevoegd

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var config = require("./config/config.json");
 var express = require("express");
+var routes = require('./routes/api_v1.js');
 
 var app = express();
 
-// Fallback - als geen enkele andere route slaagt wordt deze uitgevoerd. 
+//Gebruik de routers
+app.use('/api/v1', routes);
+
+//Fallback - als geen enkele andere route slaagt wordt deze uitgevoerd. 
 app.use('*', function(req, res) {
     res.status(400);
     res.json({
